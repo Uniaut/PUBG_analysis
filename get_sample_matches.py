7bf8ee022f66ccdf387323269742e1a1bef51089
Uniaut <kunwoo0927@gmail.com>
2022-04-30 14:29:17 +0900

    [Feature] Get match summary in get_sample_matches

--- get_sample_matches.py
+++ get_sample_matches.py
@@ -6,7 +6,14 @@ with open('my_api', mode='r') as api_key_file:
     api_key = api_key_file.read()
     print(api_key)
 
-pubg = PUBG(api_key=api_key, shard="steam")
-matches_list = pubg.samples(start=None, shard='steam').data['relationships']['matches']['data']
-match_id_list = [d['id']for d in matches_list]
-print('\n'.join(match_id_list))
+pubg = PUBG(api_key=api_key, shard='steam')
+match_list = pubg.samples(start=None, shard='steam').data['relationships']['matches']['data']
+match_id_list = [d['id']for d in match_list]
+
+for match_id in match_id_list:
+    match = pubg.match(match_id)
+    print(f'Summary of MATCH: {match_id}')
+    print(f'game_mode: {match.game_mode}')
+    print(f'map_name: {match.map_name}')
+    print(f'duration: {match.duration}')
+    print(f'telemetry_url: {match.telemetry_url}')

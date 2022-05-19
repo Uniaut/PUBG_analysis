695e1b7df3c610344ff1c5ee602df9c9dd539af4
Uniaut <kunwoo0927@gmail.com>
2022-05-19 17:49:06 +0900

    .

--- analysis/utils/kill.py
+++ analysis/utils/kill.py
@@ -4,6 +4,9 @@ import chicken_dinner.models.match as Match
 import chicken_dinner.models.telemetry as Telemetry
 from chicken_dinner.pubgapi import PUBG
 
+import analysis.utils.auth as Auth
+import analysis.samples.load as Load
+
 
 def preprocess_location(location) -> tuple:
     return tuple(map(lambda v: v / 100, (location.x, location.y, location.z)))
@@ -17,16 +20,23 @@ def get_kills(telemetry_obj: Telemetry.Telemetry):
     ]
     return modded_events
 
+def test(telemetry_obj: Telemetry.Telemetry):
+    modded_events = [
+        (preprocess_location(event.killer.location), preprocess_location(event.victim.location), event)
+        for event in telemetry_obj.filter_by('log_player_kill_v2')
+        if event.killer is not None
+    ]
+    for a, b, event in modded_events:
+        if a:
+            print([event])
+    return modded_events
+
 if __name__ == "__main__":
-    api_key = None
-    with open(r".\my_api", mode="r") as api_key_file:
-        api_key = api_key_file.read()
-        print(api_key)
+    pubg = Auth.pubg()
 
-    pubg = PUBG(api_key=api_key, shard="steam")
-
-    sample_match_id = "ca16964d-f44b-4714-b40c-78bb8607b688"
-    match = pubg.match(sample_match_id)
-    telem = match.get_telemetry()
-
-    get_kills(telem)
+    samples = pubg.samples().match_ids[:2]
+    for s_id in samples:
+        print(f'Loading {s_id}')
+        match = Load.load_match(pubg, s_id)
+        telemetry = Load.load_telemetry(match)
+        test(telemetry)
